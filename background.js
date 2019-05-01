1e2ea0de493d8758bbe20d2074b8f42e55ac230d
Alex Tasco <atasco@ptc.com>
2019-05-01 12:06:13 -0400

    updated icons, cleaned up code, updated readme

--- background.js
+++ background.js
@@ -2,4 +2,5 @@ function doIt() {
 	browser.tabs.executeScript({ file: "sp_count.js"});
 }
 
+// The below line adds a listener to when the extension icon is clicked on the browser tool bar. The doIt() function simply executes another script in the context of the active tab. 
 browser.browserAction.onClicked.addListener(doIt);
--- sp_count.js
+++ sp_count.js
@@ -1,15 +1,19 @@
-console.log("clicked");
 var total = 0;
-	
-var point_values = document.getElementsByClassName("customfield_10013");
-count = point_values.length;
 
-for (var i = 0; i < count; i++) {
-	console.log("loop");
+// In my specific instance I am working with a cloud Atlassian hosted Jira instance and I have found that the Story Point values are presented inside of a <td> tag with class name "customfield_10013". I have not tested on any other instance of Jira so you may need to modify the below line to work with your particular instance of Jira. 
+var point_values = document.getElementsByClassName("customfield_10013");
+var wrong_page = 0;
+if (point_values == null || point_values.length < 1) {
+	wrong_page = 1;
+}
+
+for (var i = 0; i < point_values.length; i++) {
 	if (point_values[i].childNodes[0] != null) {
 		total += parseFloat(point_values[i].childNodes[0].data.trim());
 	}
 }
-
-alert("Story Points: " + total);
-
+if (!wrong_page){ 
+	alert("Story Points: " + total);
+} else {
+	alert("No Story Points found.  Ensure that you sure you are viewing the results of a query in Jira and that \"Story Points\" is added as a column.");
+}
